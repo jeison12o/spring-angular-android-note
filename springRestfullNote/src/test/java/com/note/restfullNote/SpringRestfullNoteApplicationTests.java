31185c650fbe61c7d1ce544e9376bf56e4d62d0a
jeyson ortiz perez <jeisonortis12@gmail.com>
2020-07-23 22:41:11 +0200

    creamos el proyecto de spring boot

---
+++ springRestfullNote/src/test/java/com/note/restfullNote/SpringRestfullNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.restfullNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfullNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
